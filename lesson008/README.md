c41226de193bb3507dd5989d5d75192f1ee43d93
Takuya Sato <business@satotakuya.jp>
2019-06-05 07:18:54 +0900

    lesson

---
+++ lesson008/README.md
@@ -0,0 +1,50 @@
+! Tutorial
+
+Vue.js + Webpack + Truffle = Wow!
+
+~~~ bash terminal
+mkdir lesson008
+cd lesson008
+truffle init
+truffle compile
+truffle migrate --reset
+~~~
+
+~~~ bash terminal
+vue init webpack-simple client
+cd client
+npm install
+npm run dev
+code src App.vue
+rm -rf node_modules/websocket/.git
+npm i -D babel-polyfill
+npm i -D babel-plugin-transform-regenerator
+code .babelrc
+code webpack.config.js
+npm i -D web3@1.0.0-beta.37
+code src App.vue
+npm i -D json-loader
+npm i -D truffle
+npm i -D truffle-solidity-loader
+code webpack.config.js
+code package.json
+cd ..
+touch contracts/Hello.sol
+code contracts/Hello.sol
+truffle compile
+touch migrations/2_deploy.js
+code migrations/2_deploy.js
+truffle migrate
+ln -s `pwd`/../build/contracts src/assets
+cd client
+npm i -D truffle-contract
+code src/App.vue
+npm run dev
+npm run build
+lite-server
+rm -rf node_modules/ganache-core/node_modules/web3-providers-ws/node_modules/websocket/.git
+npm i -D uglifyjs-webpack-plugin@1
+code webpack.config.js
+npm run publish
+http-server
+~~~
---
+++ lesson008/client/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Web3 from "web3";
+import TruffleContract from "truffle-contract";
+import Artifacts from "./assets/contracts/Hello.json";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Say from Solidity!"
+    };
+  },
+  async mounted() {
+    console.log(Web3);
+    let provider = new Web3.providers.HttpProvider("http://localhost:7545");
+    let web3 = new Web3(provider);
+    let accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    let abs = new TruffleContract(Artifacts);
+    abs.setProvider(provider);
+    let network = Object.keys(Artifacts.networks)[0];
+    let address = Artifacts.networks[network].address;
+    let hello = await abs.at(address);
+    this.msg = await hello.sayHello();
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
